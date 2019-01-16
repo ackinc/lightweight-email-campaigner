3cd24291d327d37aec75dd29b45633cb60b8c06e
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2019-01-17 02:06:51 +0530

    Added helper methods to wrangle google's oauth tokens

---
+++ services/oauthClient.js
@@ -0,0 +1,30 @@
+const { OAuth2Client } = require('google-auth-library');
+
+const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI } = process.env;
+const client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+
+async function exchangeAuthCodeForTokens(code) {
+  const r = await client.getToken(code);
+
+  return {
+    accessToken: r.tokens.access_token,
+    refreshToken: r.tokens.refresh_token,
+    idToken: r.tokens.id_token,
+  };
+}
+
+async function getUserDetailsFromIdToken(id_token) {
+  const ticket = await client.verifyIdToken({ idToken: id_token, audience: CLIENT_ID });
+  const payload = await ticket.getPayload();
+
+  return {
+    firstname: payload.given_name,
+    lastname: payload.family_name,
+    email: payload.email,
+  };
+}
+
+module.exports = {
+  exchangeAuthCodeForTokens,
+  getUserDetailsFromIdToken,
+};
