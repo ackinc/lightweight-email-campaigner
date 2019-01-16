0f8419380202c14800186ab840192ae2ddaf5137
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2019-01-17 01:56:40 +0530

    Added helper methods for jwt generation and decoding

---
+++ services/jwt.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const { JWT_SECRET } = process.env;
+
+function generate(payload, expiresIn = '14 days') {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, JWT_SECRET, { expiresIn }, (err, token) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(token);
+    });
+  });
+}
+
+function decode(token) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(decoded);
+    });
+  });
+}
+
+module.exports = {
+  generate,
+  decode,
+};
