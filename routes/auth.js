f3ec61c46a5778e813f85c3a6b4210036fee520a
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2019-01-17 10:24:12 +0530

    No longer need refresh and access tokens since we're no longer relying on the gmail api to send emails

--- routes/auth.js
+++ routes/auth.js
@@ -8,13 +8,9 @@ const router = express.Router();
 
 // This route is hit when a user signs-in with Google in the front-end
 router.post('/', async (req, res, next) => {
-  const { authCode } = req.body;
+  const { idToken } = req.body;
 
   try {
-    const {
-      accessToken, refreshToken, idToken,
-    } = await oauthClientService.exchangeAuthCodeForTokens(authCode);
-
     // Create the user if not already in DB
     const {
       firstname, lastname, email,
@@ -25,15 +21,6 @@ router.post('/', async (req, res, next) => {
 
     const token = await tokenService.generate({ id: user.id, email });
     res.json({ token });
-
-    // if this user has already authorized our app,
-    //   google will not supply another refresh token
-    user.refreshToken = refreshToken || user.refreshToken;
-    user.accessToken = accessToken;
-
-    // TODO: if this call fails for a new user, we'll need to find
-    //   some other way of getting a refresh token for him/her
-    user.save();
   } catch (err) {
     next(err);
   }
