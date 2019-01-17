90912a5b3eefd75ff1c3b563f1c9106b7b3c6a4e
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2019-01-17 10:04:46 +0530

    Replaced nodemailer with sendgrid

--- services/campaign.js
+++ services/campaign.js
@@ -1,46 +1,15 @@
-const urljoin = require('url-join');
-
 const db = require('../db');
 const { sendMails } = require('./mail');
-const { getRandomString } = require('../utils');
-
-function injectTracker(mailBody, tracker) {
-  const trackerEndpoint = urljoin(process.env.BACKEND_URL, `/tracker/${tracker}`);
-  return `${mailBody}<img src="${trackerEndpoint}" />`;
-}
 
 function executeCampaign(user, campaign, leads) {
   const { subject, body } = campaign;
-  const trackers = {};
 
-  // see type definition of mailsToSend in services/mail.js
-  const mailsToSend = leads.map((lead) => {
-    const tracker = getRandomString(20);
-    trackers[lead.id] = tracker;
-    return ({
-      subject,
-      body: injectTracker(body, tracker),
-      lead,
-    });
-  });
+  sendMails(user.email, leads.map(l => l.email), subject, body);
 
-  sendMails(user, mailsToSend, ({ success, failure }) => {
-    // create CampaignLead records as appropriate
-
-    db.models.CampaignLead.bulkCreate(success.map(lead => ({
-      campaign_id: campaign.id,
-      lead_id: lead.id,
-      delivered: true,
-      tracker: trackers[lead.id],
-    })));
-
-    db.models.CampaignLead.bulkCreate(failure.map(lead => ({
-      campaign_id: campaign.id,
-      lead_id: lead.id,
-      delivered: false,
-      tracker: trackers[lead.id],
-    })));
-  });
+  db.models.CampaignLead.bulkCreate(leads.map(lead => ({
+    campaign_id: campaign.id,
+    lead_id: lead.id,
+  })));
 }
 
 module.exports = { executeCampaign };
--- services/mail.js
+++ services/mail.js
@@ -1,51 +1,16 @@
-const after = require('lodash/after');
-const nodemailer = require('nodemailer');
+const sgMail = require('@sendgrid/mail');
 
-// Purpose: Send emails and report on delivery success
-// Params:
-//   sender      -> User
-//   mailsToSend -> array | [mailToSend]
-//   mailToSend  -> dict | { lead: Lead, subject: string, body: string }
-//   cb will be called with a dict | {
-//     success: [Lead],
-//     failure: [Lead]
-//   }
-function sendMails(sender, mailsToSend, cb) {
-  const transport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: sender.email,
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      refreshToken: sender.refreshToken,
-      accessToken: sender.accessToken,
-    },
-  });
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  const success = []; // Leads to which mail delivery succeeded
-  const failure = []; // Leads to which mail delivery failed
-  const complete = after(mailsToSend.length, () => cb({ success, failure }));
-
-  mailsToSend.forEach(({ lead, subject, body }) => {
-    const mailOpts = {
-      from: sender.email,
-      to: lead.email,
-      subject,
-      html: body,
-    };
-    transport.sendMail(mailOpts, (err) => {
-      if (err) {
-        failure.push(lead);
-        console.error(err); // eslint-disable-line no-console
-      } else {
-        success.push(lead);
-      }
-      complete();
-    });
-  });
+function sendMails(sender, recipients, subject, body) {
+  const msg = {
+    from: sender,
+    to: recipients,
+    subject,
+    text: body,
+  };
+  sgMail.sendMultiple(msg)
+    .catch(e => console.error(e)); // eslint-disable-line no-console
 }
 
-module.exports = {
-  sendMails,
-};
+module.exports = { sendMails };
