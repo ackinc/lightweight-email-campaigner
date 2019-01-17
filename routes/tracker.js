bb594f561458312c9687ce3aa24a259ba0f36851
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2019-01-18 03:02:52 +0530

    Minor refactor + better comment for /tracker route

--- routes/tracker.js
+++ routes/tracker.js
@@ -1,21 +1,24 @@
-const tz = require('moment-timezone');
 const express = require('express');
+const tz = require('moment-timezone');
 
 const db = require('../db');
 
 const router = express.Router();
 
 // This route is the webhook we're providing to sendgrid for event capturing
+// Input
+//   req.body - an array of event objects; each event has this shape { event, tracker, timestamp }
 router.post('/', (req, res) => {
   res.end();
 
   const events = req.body;
+  const allowedEvents = ['delivered', 'open'];
   events.forEach(({ event, tracker, timestamp }) => {
-    const allowedEvents = ['delivered', 'open'];
     if (!allowedEvents.includes(event)) return;
 
     const fieldToUpdate = event === 'delivered' ? 'deliveredAt' : 'openedAt';
     const eventTime = tz(timestamp * 1000).utc().format('YYYY-MM-DD HH:mm:ss');
+
     db.query(`UPDATE campaignleads
               SET ${fieldToUpdate}='${eventTime}', updatedAt=UTC_TIMESTAMP()
               WHERE tracker='${tracker}' AND ${fieldToUpdate} IS NULL`);
