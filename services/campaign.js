c9f8f68d8686f93f814baed5553feb32f1de5138
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2019-01-18 03:29:55 +0530

    Made the campaign service's executeCampaign an async function for better error handling in caller

--- services/campaign.js
+++ services/campaign.js
@@ -2,25 +2,27 @@ const db = require('../db');
 const { sendPersonalizedMails } = require('./mail');
 const { getRandomString } = require('../utils');
 
-function executeCampaign(userEmail, campaign, leads) {
-  const { subject, body } = campaign;
+// Adds trackers for each mail, inserts appropriate campaign-lead links in DB
+//   and sends the emails
+async function executeCampaign(userEmail, campaign, leads) {
+  const { id: campaignId, subject, body } = campaign;
   const trackers = {};
   const trackerSize = 20;
 
   leads.forEach((l) => { trackers[l.id] = getRandomString(trackerSize); });
 
-  sendPersonalizedMails(
+  await db.models.CampaignLead.bulkCreate(leads.map(l => ({
+    campaignId,
+    leadId: l.id,
+    tracker: trackers[l.id],
+  })));
+
+  await sendPersonalizedMails(
     userEmail,
     leads.map(l => ({ to: l.email, customArgs: { tracker: trackers[l.id] } })),
     subject,
     body,
   );
-
-  db.models.CampaignLead.bulkCreate(leads.map(l => ({
-    campaignId: campaign.id,
-    leadId: l.id,
-    tracker: trackers[l.id],
-  })));
 }
 
 module.exports = { executeCampaign };
