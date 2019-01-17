af9ee564cf2c97a68fa7bca28e9f78cf2e58b05b
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2019-01-17 14:18:49 +0530

    Modified tracker route to listen for sendgrid's event webhooks

--- routes/tracker.js
+++ routes/tracker.js
@@ -1,16 +1,22 @@
+const tz = require('moment-timezone');
 const express = require('express');
 
 const db = require('../db');
 
 const router = express.Router();
 
-// This route is hit whenever a lead opens an email we sent them
-router.get('/:id', (req, res) => {
-  db.query(`UPDATE campaignleads
-            SET delivered=true, openAt=NOW(), updatedAt=NOW()
-            WHERE tracker='${req.params.id}'
-            AND openAt IS NULL`);
+// This route is the webhook we're providing to sendgrid for event capturing
+router.get('/', (req, res) => {
   res.end();
+
+  const events = req.body;
+  events.forEach(({ event, tracker, timestamp }) => {
+    const fieldToUpdate = event === 'delivered' ? 'deliveredAt' : 'openedAt';
+    const eventTime = tz(timestamp * 1000).utc().format('YYYY-MM-DD HH:mm:ss');
+    db.query(`UPDATE campaignleads
+              SET ${fieldToUpdate}='${eventTime}', updatedAt=UTC_TIMESTAMP()
+              WHERE tracker='${tracker}' AND ${fieldToUpdate} IS NULL`);
+  });
 });
 
 module.exports = router;
