eacd4d8279798ce79081144dca8635a8d835b02b
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2021-05-15 15:35:34 +0530

    Fixed open tracking

--- routes/tracker.js
+++ routes/tracker.js
@@ -1,41 +1,21 @@
 const express = require("express");
 const tz = require("moment-timezone");
+const path = require("path");
 
 const db = require("../common/db");
 
 const router = express.Router();
 
-// This route is the webhook we're providing to sendgrid for event capturing
-// Input
-//   req.body - an array of event objects; each event has this shape { event, tracker, timestamp }
-router.post("/", (req, res) => {
-  res.end();
+router.get("/:tracker", async (req, res) => {
+  res.sendFile(path.join(__dirname, "../pixel.png"));
 
-  const events = req.body;
-  const allowedEvents = ["delivered", "open"];
-  events.forEach(({ event, tracker, timestamp }) => {
-    if (!allowedEvents.includes(event)) return;
-    if (!tracker) return;
-
-    const fieldToUpdate = event === "delivered" ? "deliveredAt" : "openedAt";
-    const eventTime = tz(timestamp * 1000)
-      .utc()
-      .format("YYYY-MM-DD HH:mm:ss");
-
-    db.query(
-      `
-      UPDATE campaignleads
-      SET "${fieldToUpdate}" =:eventTime, "updatedAt"=now()
-      WHERE tracker=:tracker AND "${fieldToUpdate}" IS NULL
-    `,
-      { replacements: { eventTime, tracker } }
-    );
-  });
-});
-
-router.get("/:tracking_id", (req, res) => {
-  // TODO: implement
-  res.end();
+  const { tracker } = req.params;
+  await db.models.CampaignLead.update(
+    {
+      openedAt: tz(new Date()).format("YYYY-MM-DD HH:mm:ss"),
+    },
+    { where: { tracker } }
+  );
 });
 
 module.exports = router;
